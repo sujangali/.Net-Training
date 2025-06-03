76bb22b7a264b60a5677b4e1a630b93c87393717
sujangali <sujan.fullstack@gmail.com>
2025-06-03 17:49:09 +0530

    error handling

---
+++ java Script/error.js
@@ -0,0 +1,21 @@
+// try{
+//     let num = 5;
+//     console.log(num.toUpperCase());
+// }
+// catch(error){
+//     console.log("error occured",error.message);
+// }
+// finally{
+//     console.log("execution completed");
+// }
+try{
+    var b = 20;
+    var result = a*b;
+    console.log(result);
+}
+catch(error){
+    console.log(error.message);
+}
+finally{
+    console.log("the code is executed");
+}
